765ad1a237a39f7a1074a87fd0b3365f05e3c1de
cleonavila <cleon@almagrario.com>
2025-08-28 09:29:49 -0500

    Ajustes

---
+++ src/app/products/components/product-card/product-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+@Component({
+  selector: 'app-product-card',
+  imports: [RouterLink],
+  templateUrl: './product-card.component.html',
+  styles: ``
+})
+export class ProductCardComponent {
+
+}
--- src/app/store-front/pages/home/home.component.ts
+++ src/app/store-front/pages/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ProductComponent } from "../product/product.component";
+import { ProductCardComponent } from "../../../products/components/product-card/product-card.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ProductCardComponent],
   templateUrl: './home.component.html',
   styles: ``
 })
