65c6f0bf91c51d763a1f83fbbab010c5a6a3edf1
cleonavila <cleon@almagrario.com>
2025-09-25 13:58:24 -0500

    Ajustes

--- src/app/store-front/pages/home/home.component.ts
+++ src/app/store-front/pages/home/home.component.ts
@@ -1,23 +1,37 @@
 import { Component, inject } from '@angular/core';
 import { ProductCardComponent } from "../../../products/components/product-card/product-card.component";
 import { ProductService } from '@products/services/product.service';
-import { rxResource } from '@angular/core/rxjs-interop';
-import { ProductImagePipe } from '@products/pipes/product-image.pipe';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { PaginationComponent } from "@shared/components/pagination/pagination.component";
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-home',
-  imports: [ProductCardComponent],
+  imports: [ProductCardComponent, PaginationComponent],
   templateUrl: './home.component.html',
   styles: ``
 })
 export class HomeComponent {
 
-  productService = inject(ProductService)
+  productService = inject(ProductService);
+
+  activatedRoute = inject(ActivatedRoute);
+
+  currentPage = toSignal(this.activatedRoute.queryParamMap.pipe(
+    map(params => params.get('page') ? +params.get('page') ! : 1),
+    map(page => (isNaN(page) ? 1 : page))
+  ), {
+    initialValue: 1
+  });
 
   productResource = rxResource({
-    request: () => ({}),
+    request: () => ({ page: this.currentPage() - 1}),
     loader: ({request}) => {
-      return this.productService.getProducts({limit: 9});
+      return this.productService.getProducts({
+        limit: 9,
+        offset: request.page * 9
+      });
     }
   })
 }
